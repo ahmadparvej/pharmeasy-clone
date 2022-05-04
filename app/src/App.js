55b3e698474c4585d3053e6358054a1ecd131e3d
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-05-04 15:39:54 +0530

    add

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
 
-import Button from '@mui/material/Button';
+
 function App() {
   return (
     <>
     <h1>pharmeasy.com</h1>  
-    <Button variant="contained">Hello World</Button>
+   
     </>
   );
 }
