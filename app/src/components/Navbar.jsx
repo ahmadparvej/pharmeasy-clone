2faedf96ed1a940ea3229e1d43d5989ac67f880d
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-05-04 22:00:01 +0530

    add

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -4,8 +4,13 @@ import Styles from "./Navbar.module.css"
 const Navbar = () => {
   return (
     <div className={Styles.navbar}>
-      <div>
-
+      <div className={Styles.container_navbar}>
+         <img className={Styles.main_logo} src='https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png' alt='logo'></img>
+         <div className={Styles.inputBox}>
+           <input className={Styles.pincode} type='number' placeholder='PIN CODE'></input>
+           <h1 className={Styles.straightbar}>|</h1>
+           <input className={Styles.searchbar} type="text" placeholder='Search Medicines/Healthcare products @18% Off'/>
+         </div>
       </div>
       <div>
 
