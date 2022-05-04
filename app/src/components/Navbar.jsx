0dee962cb3882ec3ffb2530096024ddb13ef19cd
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-05-04 15:23:57 +0530

    add

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Styles from "Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={Styles.navbar}>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
