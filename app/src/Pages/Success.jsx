52d31317697981d359a79b72fc5aed2a26c8d062
Chiranjit Behera <99542444+Chiranjit34@users.noreply.github.com>
2022-05-08 01:59:53 +0530

    minor

--- app/src/Pages/Success.jsx
+++ app/src/Pages/Success.jsx
@@ -14,13 +14,17 @@ function Success() {
 
   return (
     <Box>
-      <Image
+      {/* <Image
         src="https://www.clipartmax.com/png/full/179-1795386_patient-success-success-icon-png.png"
         alt=""
+      /> */}
+      <Image
+        src="https://c.tenor.com/0AVbKGY_MxMAAAAC/check-mark-verified.gif"
+        alt="green tick"
       />
       <h2>Thank you for your purchase!</h2>
       <p>
-              Your order id: <span id="order">{id }</span>
+        Your order id: <span id="order">{id}</span>
       </p>
 
       <Button onClick={handleClick}>Continue Shopping</Button>
@@ -29,5 +33,3 @@ function Success() {
 }
 
 export default Success;
-
-// onclick = { handleClick };
