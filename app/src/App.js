927521592581c9b0490ae4903fec8b613ccd5d82
Chiranjit Behera <99542444+Chiranjit34@users.noreply.github.com>
2022-05-07 14:52:43 +0530

    update

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-// import OTP from "./Pages/OTP"
+import OTP from "./Pages/OTP"
 
 function App() {
   return (
@@ -16,9 +16,9 @@ function App() {
         <Route path="/healthcare-products" element={<HealthCareProducts />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/otp" element={<OTP />} />
       </Routes>
       <Footer />
-      {/* <OTP/> */}
     </div>
   );
 }
