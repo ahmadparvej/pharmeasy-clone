dc965d1d9b3b822f8f529476771c768c8e49e1ab
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-05-09 11:46:12 +0530

    add

--- app/src/Pages/Homepage.jsx
+++ app/src/Pages/Homepage.jsx
@@ -3,12 +3,14 @@ import FooterHome from '../components/FooterHome'
 import FooterMiddle from '../components/FooterMiddle'
 // import Navbar from "../components/Navbar"
 import DemoCarousel from '../components/Slider'
+import SwipeToSlide from './MiddleSlider'
 
 const Homepage = () => {
   return (
     <div>
       {/* <Navbar/> */}
       <DemoCarousel/>
+      <SwipeToSlide/>
       <FooterHome/>
       <FooterMiddle/>
     </div>
