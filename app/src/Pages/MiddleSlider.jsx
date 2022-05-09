b6b85ffeb4ff46bfa77d18876df1597d95f7cc88
Thakur Suraj Bhan Singh <99983401+Surajj07@users.noreply.github.com>
2022-05-09 11:59:21 +0530

    add

--- app/src/Pages/MiddleSlider.jsx
+++ app/src/Pages/MiddleSlider.jsx
@@ -54,31 +54,31 @@ function SampleNextArrow(props) {
     
           <Slider {...settings}>
             <div className={Styles.smallBox}>
-                <img src='https://cms-contents.pharmeasy.in/carousel_item/7e5e174fdfb-OneTouchWeb.jpg?dim=146x0&dpr=1.25&q=100' alt='image'/>
+                <img src='https://cms-contents.pharmeasy.in/carousel_item/7e5e174fdfb-OneTouchWeb.jpg?dim=146x0&dpr=1.25&q=100' alt='pic'/>
               
             </div>
             <div>
-                <img src="https://cms-contents.pharmeasy.in/carousel_item/7fdd90400a1-Featured-banner-Hansaplast-web.png?dim=146x0&dpr=1.25&q=100" alt="image"/>
+                <img src="https://cms-contents.pharmeasy.in/carousel_item/7fdd90400a1-Featured-banner-Hansaplast-web.png?dim=146x0&dpr=1.25&q=100" alt="pic"/>
         
             </div>
             <div>
-            <img src="https://cms-contents.pharmeasy.in/carousel_item/bf633425db6-web_johnson.jpg?dim=146x0&dpr=1.25&q=100" alt="image"/>
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/bf633425db6-web_johnson.jpg?dim=146x0&dpr=1.25&q=100" alt="pic"/>
         
             </div>
             <div>
-            <img src="https://cms-contents.pharmeasy.in/carousel_item/6beabc4b6ed-web_Accusure.jpg?dim=146x0&dpr=1.25&q=100" alt="image"/>
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/6beabc4b6ed-web_Accusure.jpg?dim=146x0&dpr=1.25&q=100" alt="pic"/>
     
             </div>
             <div>
-            <img src="https://cms-contents.pharmeasy.in/carousel_item/f8493a13be2-web_Pigeon.jpg?dim=146x0&dpr=1.25&q=100" alt="image"/>
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/f8493a13be2-web_Pigeon.jpg?dim=146x0&dpr=1.25&q=100" alt="pic"/>
               
             </div>
             <div>
-            <img src="https://cms-contents.pharmeasy.in/carousel_item/db82ec1a424-web_Curatio.jpg?dim=146x0&dpr=1.25&q=100" alt="image"/>
+            <img src="https://cms-contents.pharmeasy.in/carousel_item/db82ec1a424-web_Curatio.jpg?dim=146x0&dpr=1.25&q=100" alt="pic"/>
         
             </div>
             <div>
-                <img src="https://cms-contents.pharmeasy.in/carousel_item/c72feb4bd7a-web_Huggies.jpg?dim=146x0&dpr=1.25&q=100" alt="image"/>
+                <img src="https://cms-contents.pharmeasy.in/carousel_item/c72feb4bd7a-web_Huggies.jpg?dim=146x0&dpr=1.25&q=100" alt="pic"/>
             </div>
           </Slider>
         </div>
